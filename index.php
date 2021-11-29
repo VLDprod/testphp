17e90b099dafa2e9778cde137331a6d7829ce768
VLDprod <84407583+VLDprod@users.noreply.github.com>
2021-11-30 00:12:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-<h1>hey bro</h1>
+<!DOCTYPE html>
+
+<html lang="ru">
+
+<meta charset="utf-8">
+
+<link rel="stylesheet" href="kar.css"></link>
+
+<head>
+
+				<title>КАРКАДЕ</title></head>
+
+<body>
+
+	 		<header>
+
+			<div>
+
+							<ul><center>
+
+															<li><a id="href-maski" href="https://karkade.pp.ua">Главная страница</a></li>
+
+															<li><a href="https://we.karkade.pp.ua">О нас</a></li>
+
+															<li><a href="https://karkade.pp.ua/maski/index.html">О масках</a></li>
+
+															</center>
+
+							</ul>
+
+			</div>		
+
+		</header>				
+
+					
+
+</body>
+
+</html>
